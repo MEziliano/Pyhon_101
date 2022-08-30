a7de93f34dcd9efd59844a69a41552bf6bf691bc
Murilo Eziliano <77080184+MEziliano@users.noreply.github.com>
2022-08-30 17:06:07 -0300

    Add files via upload

---
+++ projects_conclued/meme_flask.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import requests
+import json
+
+
+app = Flask(__name__)
+
+def get_meme():
+  """Uncomment these two line and comment out the other url line if you want to use a
+  sr  = "/wholesomememes"
+  url = "https://meme-api.herokuapp.com/gimme"   """
+  url = "https://meme-api.heroku.com/gimme"
+  response = json.loads(requests.request("GET", url).text)
+  meme_large = response["preview"][-2]
+  subreddit = response["subreddit"]
+  return meme_large,subreddit
+
+@app.route("/")
+def index():
+  meme_pic, subreddit = get_meme()
+  return render_template("meme_index.html", meme_pic, subreddit=subreddit)
+
+app.run(host="0.0.0.0", port=80)
